a7a2c54ecc01c08164327e7b7ae14ec835a0236e
OmriGM <omrigr123@gmail.com>
2018-01-11 18:58:36 +0200

    Added stores route to navbar

--- assets/app/app-routing.module.ts
+++ assets/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {HomeComponent} from './core/home/home.component';
+import { StoresComponent } from './stores/stores.component';
 
 
 const appRoutse: Routes = [
   {path: '', component: HomeComponent},
   {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
   {path: 'shopping-list', component: ShoppingListComponent},
+  {path: 'stores', component: StoresComponent}
 ];
 
 @NgModule({
