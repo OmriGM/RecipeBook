df15d9fb47ab73854ab003e0483bec9e5814eaa2
Unknown <m.idocohen@gmail.com>
2018-01-11 20:04:14 +0200

    Bar Chart
    and footer&header html5 tags

--- assets/app/statistics/stats.component.ts
+++ assets/app/statistics/stats.component.ts
@@ -6,9 +6,26 @@ import {Component} from '@angular/core';
     styleUrls: ['./stats.component.css']
 })
 export class StatsComponent {
+    //Pie params
     public pieChartLabels:string[] = ['Meat', 'Milk', 'Sea food'];
     public pieChartData:number[] = [300, 500, 100];
     public pieChartType:string = 'pie';
+
+
+    //Graph params
+    public barChartOptions:any = {
+        scaleShowVerticalLines: false,
+        responsive: true,
+        maintainAspectRatio: false
+    };
+    public barChartLabels:string[] = ['Rishon', 'Nes Ziona', 'TLV', 'Moshe', 'Ma?', 'ok', 'bye'];
+    public barChartType:string = 'bar';
+    public barChartLegend:boolean = false;
+
+    public barChartData:any[] = [
+        {data: [2, 4, 2, 3, 5, 1, 2], label: 'Series A'}
+    ];
+    www = 0.1;
 
     // events
     public chartClicked(e:any):void {
