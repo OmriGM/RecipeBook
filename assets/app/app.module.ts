edbe718ebebb88cae85cc5f91de79c88d3347a37
Unknown <m.idocohen@gmail.com>
2018-01-11 16:16:22 +0200

    New componenet footer
    Video on home component.

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {AppRoutingModule} from './app-routing.mosule';
+import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
--- assets/app/core/core.module.ts
+++ assets/app/core/core.module.ts
@@ -2,24 +2,27 @@ import {NgModule} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {SharedModule} from '../shared/shared.module';
-import {AppRoutingModule} from '../app-routing.mosule';
+import {AppRoutingModule} from '../app-routing.module';
 import {DataStorageService} from '../shared/data-storage.service';
 import {AuthService} from '../auth/auth.service';
 import {RecipeService} from '../shared/recipe.service';
 import {ShoppingListService} from '../shared/shoppingList.service';
+import {FooterComponent} from "./footer/footer.component";
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    HomeComponent
-    ],
+    HomeComponent,
+    FooterComponent
+  ],
   imports: [
     SharedModule,
     AppRoutingModule
   ],
   exports: [
     AppRoutingModule,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   providers: [ShoppingListService, RecipeService, AuthService, DataStorageService]
 })
