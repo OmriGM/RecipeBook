22cb8b1e2677f065474ec5a4626dc78c3e550b94
Unknown <m.idocohen@gmail.com>
2018-01-14 22:34:52 +0200

    fixed home page
    Font & dynamic css

--- assets/app/recipes/recipe.model.ts
+++ assets/app/recipes/recipe.model.ts
@@ -1,18 +1,5 @@
 import {Ingredient} from '../shared/ingredient.model';
 
 export class Recipe {
-  public recipeId:string;
-  public name: string;
-  public description: string;
-  public imagePath: string;
-  public ingredients: Ingredient[];
-  public catagory: string;
-
-  constructor(recipeId:string,name: string, desc: string, imagePath: string, ingredients: Ingredient[], catagory: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imagePath;
-    this.ingredients = ingredients;
-    this.catagory = catagory;
-  }
+  constructor(public recipeId:string, public name: string, public desc: string, public imagePath: string, public ingredients: Ingredient[], public catagory: string) {}
 }
